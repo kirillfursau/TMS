687257e803ad7dd273e3078682e46aee5ddd9c84
kirillfursau <krill19988@gmail.com>
2019-12-04 01:30:25 +0300

    nothing

--- src/homeWork/homeWork4/Main.java
+++ src/homeWork/homeWork4/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Сompucter cp1 = new Сompucter();
         cp1.printCompInformation();
-            cp1.turnOn();
-            cp1.turnOff();
+        cp1.turnOn();
+        cp1.turnOff();
     }
 }
