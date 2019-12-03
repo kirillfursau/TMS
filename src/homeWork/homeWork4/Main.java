e4fb154b009681fa7dc2ef1fe308d5f55e436389
kirillfursau <krill19988@gmail.com>
2019-12-04 01:27:19 +0300

    nothing

--- src/homeWork/homeWork4/Main.java
+++ src/homeWork/homeWork4/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Сompucter cp1 = new Сompucter();
         cp1.printCompInformation();
-        cp1.turnOn();
+            cp1.turnOn();
+            cp1.turnOff();
     }
 }
--- src/homeWork/homeWork4/Сompucter.java
+++ src/homeWork/homeWork4/Сompucter.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Сompucter {
     String processor;
     String randomAccessMemory;
-    String disk;
+    String drive;
     int resource;
     int count = 0;
     Scanner sc = new Scanner(System.in);
@@ -15,8 +15,8 @@ public class Сompucter {
         this.processor = sc.nextLine();
         System.out.print("Enter RAM : ");
         this.randomAccessMemory = sc.nextLine();
-        System.out.print("Enter disk : ");
-        this.disk = sc.nextLine();
+        System.out.print("Enter drive : ");
+        this.drive = sc.nextLine();
         System.out.println("Enter resource : ");
         this.resource = sc.nextInt();
     }
@@ -24,7 +24,7 @@ public class Сompucter {
     void printCompInformation() {
         System.out.println("Processor is " + processor);
         System.out.println("RAM is " + randomAccessMemory);
-        System.out.println("Disk is " + disk);
+        System.out.println("Disk is " + drive);
         System.out.println("My resource is " + resource);
     }
 
@@ -33,9 +33,14 @@ public class Сompucter {
         System.out.print("I want to turn off. Enter number : ");
         int yrNumber = sc.nextInt();
         if (yrNumber <= 1) {
-            if (yrNumber == i && resource - 1 != count) {
+            if (yrNumber == i) {
                 count++;
-                turnOn();
+                System.out.println("I am turn off.");
+                if (count == resource) {
+                    System.out.print("I am out of resource. ");
+                    count = 0;
+                    fire();
+                }
             } else {
                 fire();
             }
@@ -50,9 +55,8 @@ public class Сompucter {
         System.out.print("I want to turn on. Enter number : ");
         int yrNumber = sc.nextInt();
         if (yrNumber <= 1) {
-            if (yrNumber == i && resource - 1 != count) {
-                count++;
-                turnOff();
+            if (yrNumber == i) {
+                System.out.println("I am turn on.");
             } else {
                 fire();
             }
@@ -63,7 +67,7 @@ public class Сompucter {
     }
 
     void fire() {
-        System.out.println("I'm on fire");
-
+        System.out.println("I'm on fire. Try again");
+        turnOn();
     }
 }
