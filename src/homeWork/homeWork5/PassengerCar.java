951a9acd7011ce449ac2fec47f4ca0f759a34d8c
kirillfursau <krill19988@gmail.com>
2019-12-06 01:24:27 +0300

    nothing

--- src/homeWork/homeWork5/PassengerCar.java
+++ src/homeWork/homeWork5/PassengerCar.java
@@ -12,8 +12,17 @@ public class PassengerCar extends GroundTransport {
         this.totalPassengers = totalPassengers;
     }
 
-    String getInformation(){
-        return power + maxSpeed + weight + brand + numberOfWheels + fuelConsumption + bodyTipe + fuelConsumption+ powerKW;
+    String getInformation() {
+        return power + maxSpeed + weight + brand + numberOfWheels + fuelConsumption + bodyTipe + fuelConsumption + powerKW;
     }
-    void 
+
+    private void fuelConsumptionDistance() {
+       int fuelConsumptionDistance =  fuelConsumption;
+    }
+
+    void kiloPerFuel(int time) {
+        int distance = time * maxSpeed;
+        System.out.println("Distance on max speed is : " + distance + "for " + time + " hours");
+        System.out.println("Fuel consumption on max speed " + fuelConsumptionDistance + "for" + time + " hours");
+    }
 }
