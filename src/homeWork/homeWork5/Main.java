59743785272468c26cc83d92c81c998823ba4520
kirillfursau <krill19988@gmail.com>
2019-12-06 21:55:54 +0300

    nothing

--- src/homeWork/homeWork5/Main.java
+++ src/homeWork/homeWork5/Main.java
@@ -2,11 +2,11 @@ package homeWork.homeWork5;
 
 public class Main {
     public static void main(String[] args) {
-        PassengerCar pc1 = new PassengerCar(115,140,2300,"volvo",4,12,
-                "minivan",8);
-        Truck tr1 = new Truck(210,120,3700, "BMW",8,23,15);
-        CivilAir ca1 = new CivilAir(600,700,23000,"Airbus",32,1500,110,true);
-        WarAir wa1 = new WarAir(900,890,21000,"Killer",26,900,false,7);
+        PassengerCar pc1 = new PassengerCar(115, 140, 2300, "volvo", 4, 12,
+                "minivan", 8);
+        Truck tr1 = new Truck(210, 120, 3700, "BMW", 8, 23, 15);
+        CivilAir ca1 = new CivilAir(600, 700, 23000, "Airbus", 32, 1500, 110, true);
+        WarAir wa1 = new WarAir(900, 890, 21000, "Killer", 26, 900, false, 7);
         System.out.println(pc1.getInformation());
         System.out.println(tr1.getInformation());
         System.out.println(ca1.getInformation());
@@ -14,5 +14,7 @@ public class Main {
         pc1.kiloPerFuel(2);
         tr1.maxCarrying(6);
         ca1.maxPassengers(115);
+        wa1.shot();
+        wa1.bailout();
     }
 }
--- src/homeWork/homeWork5/WarAir.java
+++ src/homeWork/homeWork5/WarAir.java
@@ -3,14 +3,35 @@ package homeWork.homeWork5;
 public class WarAir extends AirTransport {
     boolean bailoutSystem;
     int totalRockets;
-    WarAir(int power, int maxSpeed, int weight, String brand, int wingspan, int minimumRunway, boolean bailoutSystem, int totalRockets){
+
+    WarAir(int power, int maxSpeed, int weight, String brand, int wingspan, int minimumRunway, boolean bailoutSystem, int totalRockets) {
         super(power, maxSpeed, weight, brand, wingspan, minimumRunway);
         this.totalRockets = totalRockets;
         this.bailoutSystem = bailoutSystem;
     }
-    String getInformation(){
+
+    String getInformation() {
         return "Power in horses : " + power + ". Max speed km/h : " + maxSpeed + "/ Weight(Kg) : " + weight + ". Brand : " + brand
                 + ". Wingspan(M) : " + wingspan + " Minimum distance to fly away(M) : " + minimumRunway + ". Total rockets : " + totalRockets + ". Bailout system : " +
                 bailoutSystem + ". Power in kilowat : " + powerKW();
+    }
+
+    void shot() {
+        int shots = totalRockets;
+        for (int i = 0; i <= shots; i++) {
+            if (totalRockets > 0) {
+                System.out.println("Rocket launch");
+                totalRockets--;
+            } else {
+                System.out.println("No ammo");
+            }
+        }
+    }
+    void bailout(){
+        if(bailoutSystem){
+            System.out.println("Bailout was successful");
+        }else{
+            System.out.println("No bailout system");
+        }
     }
 }
