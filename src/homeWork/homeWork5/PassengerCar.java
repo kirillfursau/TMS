e3f1060801cc3d44e59dd8aae4acd696ae88efc9
kirillfursau <krill19988@gmail.com>
2019-12-06 22:05:57 +0300

    nothing

--- src/homeWork/homeWork5/PassengerCar.java
+++ src/homeWork/homeWork5/PassengerCar.java
@@ -19,14 +19,14 @@ public class PassengerCar extends GroundTransport {
     String getInformation() {
 
         return "Power in horses : " + power + ". Max speed km/h : " + maxSpeed + ". Weight(Kg) : " + weight + ". Brand : " + brand
-                + ". Number of Wheels : " + numberOfWheels + ". Fuel consumption(L/100km) : " + fuelConsumption + ". Tipe of body : " + bodyTipe
-                + ". Power in kilowat : " + powerKW();
+                + ". Number of Wheels : " + numberOfWheels + ". Fuel consumption(L/100km) : " + fuelConsumption + ". Tipe of body : "
+                + bodyTipe +" Total passengers : " + totalPassengers + ". Power in kilowat : " + powerKW();
     }
 
 
-    void kiloPerFuel(int time) {
-        int distance = time * maxSpeed;
-        System.out.println("For the time " + time + "h car " + brand  + " moving at maximum speed " + maxSpeed + " km and use up "
+    void kiloPerFuel(double time) {
+        double distance = time * maxSpeed;
+        System.out.println("For the time " + time + "h car " + brand  + " moving at maximum speed " + maxSpeed + " it will drive " +distance + " km and use up "+
          + fuelConsumptionDistance(distance) + " liters of fuel.");
     }
 }
