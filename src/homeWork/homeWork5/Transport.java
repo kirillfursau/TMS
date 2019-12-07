04a215d264ab6a4b62a1739db92d3a829b41941e
kirillfursau <krill19988@gmail.com>
2019-12-08 01:05:36 +0300

    nothing

--- src/homeWork/homeWork5/Transport.java
+++ src/homeWork/homeWork5/Transport.java
@@ -6,7 +6,7 @@ public class Transport {
     int maxSpeed;
     int weight;
     String brand;
-    double powerKW;
+
 
     Transport(int power, int maxSpeed, int weight, String brand) {
         this.power = power;
@@ -16,7 +16,7 @@ public class Transport {
     }
 
     double powerKW() {
-        powerKW = power * 0.74;
+        double powerKW = power * 0.74;
         return powerKW;
     }
 }
