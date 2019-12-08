2338c3bb84d381e4b95e5080e11fcf962daa074f
kirillfursau <krill19988@gmail.com>
2019-12-08 16:48:54 +0300

    nothing

--- src/lessons/lesson5/Group.java
+++ src/lessons/lesson5/Group.java
@@ -4,14 +4,16 @@ public class Group {
     int id;
     Student[] students = new Student[5];
     String programmingLanguage;
-    Group[] groups = new Group[5];
+
     Group(String programmingLanguage, int id) {
         this.programmingLanguage = programmingLanguage;
         this.id = id;
     }
-     String groupInformation(){
+
+    String groupInformation() {
         return programmingLanguage + " " + id;
     }
+
     void addStudent(Student student) {
         for (int i = 0; i < students.length; i++) {
             if (students[i] == null) {
@@ -28,6 +30,20 @@ public class Group {
             }
         }
     }
+
+    static Group[] groups = new Group[5];
+//    Group[] allGroups(Group group){
+//        for (int i = 0; i < groups.length;i++){
+//            groups[i] = group;
+//        }
+//        return groups;
+//    }
+//    void printAllGroups(){
+//        for (int i = 0; i < groups.length; i++){
+//            System.out.println(groups[i]);
+//        }
+//    }
+
     void addGroup(Group group) {
         for (int i = 0; i < groups.length; i++) {
             if (groups[i] == null) {
@@ -36,12 +52,26 @@ public class Group {
             }
         }
     }
+
     void printGroupsInformation() {
         for (int i = 0; i < groups.length; i++) {
             if (groups[i] != null) {
-                System.out.println( groups[i].groupInformation());
+                System.out.println(groups[i].groupInformation());
             }
         }
+    }
+
+    Student[] checkHomeWork(int homeWorkToCheck) {
+        int count = 0;
+        Student[] checkHomeWork = new Student[count];
+        boolean[] homeWork = new boolean[5];
+        for (int i = 0; i < students.length; i++) {
+            if (homeWork[i] == true) {
+                checkHomeWork[count] = students[i];
+                count++;
+            }
+        }
+        return checkHomeWork;
     }
 }
 
--- src/lessons/lesson5/Main.java
+++ src/lessons/lesson5/Main.java
@@ -1,22 +1,41 @@
 package lessons.lesson5;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3 = new Student();
         st1.printInformation();
         System.out.println(st1.getFullName("Mr. "));
         st1.doHomeWork(2);
+        st1.doHomeWork(3);
         int[] homeWorkDone = st1.getHomeWorkDone();
         for (int i = 0; i < homeWorkDone.length; i++) {
             System.out.println(homeWorkDone[i]);
         }
+
         Group gr1 = new Group("java", 1);
+//        Group gr2 = new Group("kotlin", 6);
+//        Group gr3 = new Group("JS", 7);
+//        Group gr4 = new Group("all",0);
         gr1.addStudent(st1);
-        gr1.printGroupInformation();
-        gr1.addGroup(gr1);
-        Coach co1 = new Coach("Yauhen", "Parmon", 23, 1, 5);
-        co1.printInformation();
-        gr1.printGroupsInformation();
-        gr1.printGroupInformation();
+        gr1.addStudent(st2);
+        gr1.addStudent(st3);
+//        gr1.checkHomeWork(2);
+        System.out.println(gr1.checkHomeWork(2));
+//        System.out.println("В группе 1 ");
+//        gr1.printGroupInformation();
+//        gr2.printGroupInformation();
+//        gr3.printGroupInformation();
+//        gr3.addGroup(gr1);
+//        gr3.addGroup(gr2);
+//        gr3.addGroup(gr3);
+//        gr3.printGroupsInformation();
+//
+//        Coach co1 = new Coach("Yauhen", "Parmon", 23, 1, 5);
+//        co1.printInformation();
+
     }
 }
