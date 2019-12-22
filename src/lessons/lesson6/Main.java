d54e1374a812c6de3aa9ea2e121cd41288fcbbe5
kirillfursau <krill19988@gmail.com>
2019-12-22 17:54:39 +0300

    Nothing

--- src/lessons/lesson6/Main.java
+++ src/lessons/lesson6/Main.java
@@ -3,8 +3,9 @@ package lessons.lesson6;
 public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(3, 2);
-        Point p2 = new Point(4,3 );
-        Circle c1 = new Circle(p1, 2);
-        System.out.println(c1.containsPoint(p2));
+        Point p2 = new Point(5,3 );
+        Square square = new Square(p1,2);
+        System.out.println(square.containsPoint(p2));
+
     }
 }
