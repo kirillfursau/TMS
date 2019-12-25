fccd16643aa22763221a658f1062e69045f1fd74
kirillfursau <krill19988@gmail.com>
2019-12-25 12:00:21 +0300

    Nothing

--- src/lessons/lesson7/Main.java
+++ src/lessons/lesson7/Main.java
@@ -2,10 +2,17 @@ package lessons.lesson7;
 
 public class Main {
     public static void main(String[] args) {
-        String s1 = "kek kek kek";
+        String s1 = "202kek 2020kek 20kek lol20";
         String s2 = "asdfghj";
         String s3 = "sdadsaaaaa";
-        System.out.println(StringUtils.findLongest(s1,s2,s3));
+        System.out.println(StringUtils.findLongest(s1, s2, s3));
         System.out.println(StringUtils.kek(s1));
+        System.out.println(StringUtils.unkek(s1));
+        for (int i = 0; i < StringUtils.count20(s1).length; i++){
+            System.out.println(StringUtils.count20(s1)[i]);
+        }
+
+
     }
 }
+
--- src/lessons/lesson7/StringUtils.java
+++ src/lessons/lesson7/StringUtils.java
@@ -35,4 +35,28 @@ public class StringUtils {
     public static String kek(String s) {
         return s.replace("kek", "lol");
     }
+
+    public static String unkek(String s) {
+        return s.replace("kek", "");
+    }
+
+    public static int[] count20(String s) {
+        int index = 0;
+        int counter = 0;
+        for (int i = 0; i < s.length(); i++) {
+            s.indexOf("20", index);
+            index = s.indexOf("20", index) + 1;
+            counter++;
+            if (s.indexOf("20",index) == -1){
+                break;
+            }
+        }
+        index = 0;
+        int[] count20 = new int[counter];
+        for (int i = 0; i < count20.length; i++) {
+            count20[i] = s.indexOf("20", index);
+            index = count20[i] + 1;
+        }
+        return count20;
+    }
 }
