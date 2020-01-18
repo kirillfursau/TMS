9449855a932fe2fc38cc9c735a34c5f37db08a72
kirillfursau <krill19988@gmail.com>
2020-01-18 14:12:28 +0300

    nothing

--- src/homeWork/homeWork10/Main.java
+++ src/homeWork/homeWork10/Main.java
@@ -9,25 +9,25 @@ public class Main {
         Product apple = new Product(0, "Apple", 250);
         Product sinkers = new Product(1, "Sinkers", 15);
         Product banana = new Product(2, "Banana", 20);
-        Product pepsi = new Product(5,"pepsi",40);
-        Product cola = new Product(5,"cola", 50);
+        Product pepsi = new Product(5, "Pepsi", 40);
+        Product cola = new Product(5, "cola", 50);
         Store store = new Store();
         store.addProduct(sinkers);
         store.addProduct(apple);
         store.addProduct(pepsi);
         store.addProduct(banana);
         store.printAllProducts();
+        List<Product> priceSortList = new ArrayList<>(store.returnAllProducts());
+        Collections.sort(priceSortList);
+        for (Product product : priceSortList) {
+            System.out.println(product);
+        }
+        System.out.println();
         List<Product> sortByLastAddToList = new ArrayList<>();
         for (int i = store.returnAllProducts().size(); i > 0; i--) {
             sortByLastAddToList.add(store.returnAllProducts().get(i - 1));
         }
-        for (Product product : sortByLastAddToList){
-            System.out.println(product);
-        }
-        System.out.println();
-        List<Product> priceSortList = new ArrayList<>(store.returnAllProducts());
-        Collections.sort(priceSortList);
-        for (Product product : priceSortList){
+        for (Product product : sortByLastAddToList) {
             System.out.println(product);
         }
         System.out.println();
