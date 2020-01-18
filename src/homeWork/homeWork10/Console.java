ea4f9599fe3c114f94f6caedf64a01734fbada26
kirillfursau <krill19988@gmail.com>
2020-01-18 16:03:22 +0300

    nothing

---
+++ src/homeWork/homeWork10/Console.java
@@ -0,0 +1,9 @@
+package homeWork.homeWork10;
+
+public class Console {
+    public static void main(String[] args) {
+        Store store = new Store();
+        store.start();
+}
+}
+
--- src/homeWork/homeWork10/Store.java
+++ src/homeWork/homeWork10/Store.java
@@ -2,12 +2,16 @@ package homeWork.homeWork10;
 
 import homeWork.homeWork8.MyException;
 
+import java.io.InputStream;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Store {
     static List<Product> Products = new ArrayList<>();
+
 
     void addProduct(Product product) {
         try {
@@ -39,16 +43,18 @@ public class Store {
     void deleteProduct(int id) {
         try {
             boolean findId = false;
-            for (Product p : Products) {
-                if (p.getId() == id) {
+            int numberDeleteProduct = 0;
+            for (int i = 0; i < Products.size();i++) {
+                if (Products.get(i).getId() == id) {
                     findId = true;
+                    numberDeleteProduct = i;
                     break;
                 }
             }
             if (!findId) {
                 throw new MyException();
             }
-            Products.remove(id);
+            Products.remove(numberDeleteProduct);
             System.out.println("Delete product successful");
         } catch (MyException e) {
             System.out.println("You dont have product with this id");
@@ -86,5 +92,60 @@ public class Store {
             sortByLastAddToList.add(Products.get(i - 1));
         }
         return sortByLastAddToList;
+    }
+
+    void printFirstMenu() {
+        System.out.println("1. Вывод всех товаров");
+        System.out.println("2. Добавление товара");
+        System.out.println("3. Удаление товара");
+        System.out.println("4. Редактирование товара");
+        System.out.println("5. Выход");
+    }
+
+    Scanner sc = new Scanner(System.in);
+
+    public void start() {
+        int key;
+        if (this.sc != null) {
+            do {
+                printFirstMenu();
+                System.out.print("Введите номер меню: ");
+                key = sc.nextInt();
+                switch (key) {
+                    case 1:
+                        printAllProducts();
+                        break;
+                    case 2:
+                        System.out.print("Введите id товара: ");
+                        int id = sc.nextInt();
+                        System.out.print("Введите наименование товара: ");
+                        String name = sc.next();
+                        System.out.print("Введите цену товара: ");
+                        int price = sc.nextInt();
+                        Product product = new Product(id, name, price);
+                        addProduct(product);
+                        break;
+                    case 3:
+                        System.out.println("Введите id товара: ");
+                        int deleteId = sc.nextInt();
+                        deleteProduct(deleteId);
+                        break;
+                    case 4:
+                        System.out.println("Введите id товара для редактирования: ");
+                        int replaceId = sc.nextInt();
+                        System.out.println("Введите новое наименование товара: ");
+                        String replaceName = sc.nextLine();
+                        System.out.println("Введите новую цену товара: ");
+                        int replacePrice = sc.nextInt();
+                        Product replaceProduct = new Product();
+                        replaceProduct(replaceProduct);
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        System.out.println("Вы ввели неверное значение меню...\n");
+                }
+            } while (key != 5);
+        }
     }
 }
