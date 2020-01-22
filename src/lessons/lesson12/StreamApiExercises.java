5db7078932935acc174538fcb6d0498caf0e09ab
Dell <krill19988@gmail.com>
2020-01-22 21:25:01 +0300

    nothing

--- src/lessons/lesson12/StreamApiExercises.java
+++ src/lessons/lesson12/StreamApiExercises.java
@@ -1,6 +1,7 @@
 package lessons.lesson12;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamApiExercises {
 
@@ -10,18 +11,48 @@ public class StreamApiExercises {
     }
 
     int countShortStrings(Set<String> strings, int maxLength) {
-        List<String> strings1 = new ArrayList<>();
-        strings.stream()
-                .
+        return (int) strings.stream()
+                .filter(s -> s.length() < maxLength)
+                .count();
     }
 
     String findLongestString(Collection<String> strings) throws IllegalArgumentException {
-            return strings.stream()
-                    .sorted((s1, s2) -> s2.length() - s1.length())
-                    .findFirst()
-                    .orElseThrow(() -> {
-                        throw new IllegalArgumentException();
-                    });
-        }
+        return strings.stream()
+                .sorted((s1, s2) -> s2.length() - s1.length())
+                .findFirst()
+                .orElseThrow(() -> {
+                    throw new IllegalArgumentException();
+                });
+    }
+
+    String getFirst(List<String> strings) {
+        return strings.stream()
+                .findFirst()
+                .orElse("kek");
+    }
+
+    List<Integer> lengths(List<String> strings) {
+        return strings.stream()
+                .map(s -> s.length())
+                .collect(Collectors.toList());
+    }
+
+    void printOddNumbers(Collection<Integer> numbers) {
+        System.out.println(numbers.stream()
+                .filter(number -> number % 2 == 1)
+                .sorted((number1, number2) -> number2 - number1));
+    }
+
+    void printStringsStartingFrom(Collection<String> strings, int from) {
+        strings.stream()
+                .skip(from)
+                .forEach(string -> System.out.println(string));
+    }
+
+    List<Integer> incrementEach(List<Integer> numbers) {
+        return numbers.stream()
+                .map(number -> number++)
+                .collect(Collectors.toList());
     }
 }
+
