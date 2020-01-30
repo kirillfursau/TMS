f86974a00a682e1a4660ea58839fae31d900322d
kirillfursau <krill19988@gmail.com>
2020-01-30 13:44:13 +0300

    nothing

--- src/lessons/lesson14/Account.java
+++ src/lessons/lesson14/Account.java
@@ -1,7 +1,7 @@
 package lessons.lesson14;
 
 public class Account {
-    long balance;
+    long balance = 1000;
 
     public Account(long balance) {
         this.balance = balance;
@@ -11,19 +11,50 @@ public class Account {
 
     }
 
+    public synchronized long deposit(long amount) {
+        System.out.println(balance);
+        notifyAll();
+        return balance = balance + amount;
+    }
 
-    public static void main(String[] args) {
+    public synchronized long withdraw(long amount) {
+        if (balance < balance + amount) {
+            try {
+                System.out.println(balance);
+                wait();
+                System.out.println(balance);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return balance - amount;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         Account account = new Account(1000);
         Thread thread = new Thread(() -> {
-        for (int i = 0; i < 100_000; i++) {
-            synchronized (account){account.balance++;}
-        }});
-        Thread thread1 = new Thread(() ->{
-        for (int i = 0; i < 100_000; i++) {
-            synchronized (account){account.balance--;}
-        }});
+            for (int i = 0; i < 100_000; i++) {
+                synchronized (account) {
+                    account.balance++;
+                }
+            }
+        });
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 100_000; i++) {
+                synchronized (account) {
+                    account.balance--;
+                }
+            }
+        });
         thread.start();
+        thread.join();
         thread1.start();
+        thread1.join();
+        System.out.println(account.balance);
+        account.withdraw(100000000);
+        System.out.println(account.balance);
+        account.deposit(100000);
         System.out.println(account.balance);
     }
 }
