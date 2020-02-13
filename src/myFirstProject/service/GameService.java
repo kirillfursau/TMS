142a3fd1ffe9028d73a289d459990831fddf0ecb
kirillfursau <krill19988@gmail.com>
2020-02-13 21:54:52 +0300

    Refactor

--- src/myFirstProject/service/GameService.java
+++ src/myFirstProject/service/GameService.java
@@ -8,50 +8,37 @@ import java.util.Optional;
 
 public class GameService {
     public Optional<Player> getWinner(Game game) {
-        for (int p = 0; p < 3; p++) {
-            if ((game.getField().getFigure(p, 0) == Figure.X && game.getField().getFigure(p, 1) == Figure.X &&
-                    game.getField().getFigure(p, 2) == Figure.X) ||
-                    (game.getField().getFigure(0, p) == Figure.X && game.getField().getFigure(1, p) == Figure.X &&
-                            game.getField().getFigure(2, p) == Figure.X)) {
-                if (game.getPlayer1().getFigure() == Figure.X) {
-                    return Optional.of(game.getPlayer1());
-                } else {
-                    return Optional.of(game.getPlayer2());
-                }
-            } else if ((game.getField().getFigure(0, 0) == Figure.X && game.getField().getFigure(1, 1)
-                    == Figure.X && game.getField().getFigure(2, 2) == Figure.X) ||
-                    (game.getField().getFigure(2, 0) == Figure.X && game.getField().getFigure(1, 1)
-                            == Figure.X && game.getField().getFigure(0, 2) == Figure.X)) {
-                if (game.getPlayer1().getFigure() == Figure.X) {
-                    return Optional.of(game.getPlayer1());
-                } else {
-                    return Optional.of(game.getPlayer2());
-                }
-            }
+        if (findWinner(game, Figure.X) && game.getPlayer1().getFigure() == Figure.X) {
+            return Optional.of(game.getPlayer1());
+        } else if (findWinner(game, Figure.X) && game.getPlayer2().getFigure() == Figure.X) {
+            return Optional.of(game.getPlayer2());
         }
-        for (int p = 0; p < 3; p++) {
-            if ((game.getField().getFigure(p, 0) == Figure.O && game.getField().getFigure(p, 1) == Figure.O &&
-                    game.getField().getFigure(p, 2) == Figure.O) ||
-                    (game.getField().getFigure(0, p) == Figure.O && game.getField().getFigure(1, p) == Figure.O &&
-                            game.getField().getFigure(2, p) == Figure.O)) {
-                if (game.getPlayer1().getFigure() == Figure.O) {
-                    return Optional.of(game.getPlayer1());
-                } else {
-                    return Optional.of(game.getPlayer2());
-                }
-            } else if ((game.getField().getFigure(0, 0) == Figure.O && game.getField().getFigure(1, 1)
-                    == Figure.O && game.getField().getFigure(2, 2) == Figure.O) ||
-                    (game.getField().getFigure(2, 0) == Figure.O && game.getField().getFigure(1, 1)
-                            == Figure.O && game.getField().getFigure(0, 2) == Figure.O)) {
-                if (game.getPlayer1().getFigure() == Figure.O) {
-                    return Optional.of(game.getPlayer1());
-                } else {
-                    return Optional.of(game.getPlayer2());
-                }
-            }
+        if (findWinner(game, Figure.O) && game.getPlayer1().getFigure() == Figure.O) {
+            return Optional.of(game.getPlayer1());
+        } else if (findWinner(game, Figure.O) && game.getPlayer2().getFigure() == Figure.O) {
+            return Optional.of(game.getPlayer2());
         }
         return Optional.empty();
     }
+
+    public boolean findWinner(Game game, Figure figure) {
+        for (int p = 0; p < 3; p++) {
+            if ((game.getField().getFigure(p, 0) == figure && game.getField().getFigure(p, 1) == figure &&
+                    game.getField().getFigure(p, 2) == figure) ||
+                    (game.getField().getFigure(0, p) == figure && game.getField().getFigure(1, p) == figure &&
+                            game.getField().getFigure(2, p) == figure)) {
+                return true;
+            }
+        }
+        if ((game.getField().getFigure(0, 0) == figure && game.getField().getFigure(1, 1)
+                == figure && game.getField().getFigure(2, 2) == figure) ||
+                (game.getField().getFigure(2, 0) == figure && game.getField().getFigure(1, 1)
+                        == figure && game.getField().getFigure(0, 2) == figure)) {
+            return true;
+        }
+        return false;
+    }
+
 
     public boolean isOver(Game game, FieldService fieldService) {
         if (!getWinner(game).isPresent() && fieldService.isFull(game.getField()) == false) {
@@ -60,7 +47,7 @@ public class GameService {
         fieldService.draw(game.getField());
         if (getWinner(game).isPresent()) {
             System.out.println("The winner is " + getWinner(game).get().getName());
-        }else{
+        } else {
             System.out.println("Draw");
         }
         return true;
