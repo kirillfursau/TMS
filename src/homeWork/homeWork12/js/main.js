c7f5ac97d20e1dc820bc08b600be1b5ff0b11146
kirillfursau <krill19988@gmail.com>
2020-02-22 17:13:31 +0300

    done

--- src/homeWork/homeWork12/js/main.js
+++ src/homeWork/homeWork12/js/main.js
@@ -11,14 +11,19 @@ $(document).on('click', '#submit-btn', function (e) {
         success: function (data) {
             console.log('запрос успешно получен');
             console.log(data);
-            $('#films').empty();
+            $('#myTable').empty();
+            // $('#myTable').append('<tr id=tr_' + el.title + '><td>overview</td></tr>');
             data.results.forEach(el => {
                 const name = document.createElement("film");
                 name.src = el.title;
                 if (el.title != null) {
-                    $('#films').append('Name = ' + el.title + ', Type  = ' + el.media_type + "<br>");
+                    // $("#myTable > tbody").append("<tr><td>row content</td></tr>");
+                    $('#myTable').append('<tr id=tr_' + el.title + '><td>' + el.title + '</td><td>'
+                        + el.media_type + '</td><td>' + el.overview+ '</td></tr>');
+                    // $('#myTable').after('<tr>' + el.title + '</tr><tr>' + el.media_type + '</tr>');
                 } else {
-                    $('#films').append('Name = ' + el.name + ', Type  = ' + el.media_type + "<br>");
+                    $('#myTable').append('<tr id=tr_' + el.name + '><td>' + el.name + '</td><td>'
+                        + el.media_type + '</td><td>' + el.overview+ '</td></tr>');
                 }
             });
             jQuery('#films').append('<a href="https://www.google.com/" style="font-size: 18px">For more info go to google</a>');
@@ -27,6 +32,6 @@ $(document).on('click', '#submit-btn', function (e) {
 // jQuery('document').ready(function () {
 });
 $(document).on('click', '#clear-btn', function (e) {
-    $('#films').empty();
+    $('#myTable').empty();
 });
 // });
