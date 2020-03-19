7c392b6d7ccee3951550c204d9a2f949d3b139c3
kirillfursau <krill19988@gmail.com>
2020-03-19 15:22:09 +0300

    fix

--- firstSpring/src/main/java/impl/UserInputImpl.java
+++ firstSpring/src/main/java/impl/UserInputImpl.java
@@ -3,6 +3,7 @@ package impl;
 import interfaces.UserInput;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Scanner;
 
 @Component
@@ -12,9 +13,12 @@ public class UserInputImpl implements UserInput {
     private String operator;
     private Scanner scanner;
 
-
     public UserInputImpl() {
-        Scanner scanner = new Scanner(System.in);
+    }
+
+    @PostConstruct
+    public void initialization() {
+        scanner = new Scanner(System.in);
         System.out.print("Input first number : ");
         firstNumber = scanner.nextDouble();
         System.out.print("Input second number : ");
